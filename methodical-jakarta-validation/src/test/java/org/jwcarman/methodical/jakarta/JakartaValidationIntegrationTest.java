46bf9a6d89788b2f0a062a6f863a50ee55705480
James Carman <jcarman@callibrity.com>
2026-04-18 09:47:26 -0400

    test(jakarta): end-to-end invocation through DefaultMethodInvokerFactory

---
+++ methodical-jakarta-validation/src/test/java/org/jwcarman/methodical/jakarta/JakartaValidationIntegrationTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2026 James Carman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jwcarman.methodical.jakarta;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.groups.Default;
+import java.lang.reflect.Method;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.jwcarman.methodical.Argument;
+import org.jwcarman.methodical.MethodInvoker;
+import org.jwcarman.methodical.def.DefaultMethodInvokerFactory;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class JakartaValidationIntegrationTest {
+
+  public static class Greeter {
+    public String greet(@Argument @NotBlank String name) {
+      return "hello " + name;
+    }
+  }
+
+  private MethodInvoker<String> buildInvoker() throws Exception {
+    JakartaMethodValidatorFactory vf =
+        new JakartaMethodValidatorFactory(
+            Validation.buildDefaultValidatorFactory().getValidator(),
+            new DefaultValidationGroupResolver(new Class<?>[] {Default.class}, true));
+    DefaultMethodInvokerFactory factory = new DefaultMethodInvokerFactory(List.of(), vf);
+    Method m = Greeter.class.getDeclaredMethod("greet", String.class);
+    return factory.create(m, new Greeter(), String.class);
+  }
+
+  @Test
+  void valid_invocation_returns_result() throws Exception {
+    assertThat(buildInvoker().invoke("world")).isEqualTo("hello world");
+  }
+
+  @Test
+  void invalid_argument_surfaces_constraint_violation_exception() throws Exception {
+    MethodInvoker<String> invoker = buildInvoker();
+    assertThatThrownBy(() -> invoker.invoke("")).isInstanceOf(ConstraintViolationException.class);
+  }
+}
