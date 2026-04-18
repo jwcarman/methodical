d192e62804c7af87859ee6122fd6306fa3cc53e6
James Carman <jcarman@callibrity.com>
2026-04-18 09:38:15 -0400

    feat(jakarta): add ValidationGroupResolver with hierarchy-aware defaults

---
+++ methodical-jakarta-validation/src/main/java/org/jwcarman/methodical/jakarta/DefaultValidationGroupResolver.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2026 James Carman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jwcarman.methodical.jakarta;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public final class DefaultValidationGroupResolver implements ValidationGroupResolver {
+
+  private final Class<?>[] defaultGroups;
+  private final boolean defaultValidateReturnValue;
+
+  public DefaultValidationGroupResolver(
+      Class<?>[] defaultGroups, boolean defaultValidateReturnValue) {
+    this.defaultGroups = Objects.requireNonNull(defaultGroups, "defaultGroups").clone();
+    this.defaultValidateReturnValue = defaultValidateReturnValue;
+  }
+
+  @Override
+  public Class<?>[] resolveGroups(Object target, Method method) {
+    MethodValidation annotation = findAnnotation(target, method);
+    if (annotation != null && annotation.groups().length > 0) {
+      return annotation.groups();
+    }
+    return defaultGroups.clone();
+  }
+
+  @Override
+  public boolean shouldValidateReturnValue(Object target, Method method) {
+    MethodValidation annotation = findAnnotation(target, method);
+    if (annotation != null) {
+      return annotation.validateReturnValue();
+    }
+    return defaultValidateReturnValue;
+  }
+
+  private MethodValidation findAnnotation(Object target, Method method) {
+    MethodValidation onMethod = AnnotationFinder.findOnMethod(method, MethodValidation.class);
+    if (onMethod != null) {
+      return onMethod;
+    }
+    Class<?> type = target != null ? target.getClass() : method.getDeclaringClass();
+    return AnnotationFinder.findOnClass(type, MethodValidation.class);
+  }
+}
---
+++ methodical-jakarta-validation/src/test/java/org/jwcarman/methodical/jakarta/DefaultValidationGroupResolverTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2026 James Carman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jwcarman.methodical.jakarta;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.validation.groups.Default;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class DefaultValidationGroupResolverTest {
+
+  interface GroupA {}
+
+  interface GroupB {}
+
+  static class Plain {
+    public void run() {}
+  }
+
+  @MethodValidation(groups = GroupA.class)
+  static class ClassAnnotated {
+    public void run() {}
+
+    @MethodValidation(groups = GroupB.class, validateReturnValue = false)
+    public void other() {}
+  }
+
+  @MethodValidation
+  static class EmptyGroups {
+    public void run() {}
+  }
+
+  @Test
+  void uses_constructor_default_when_nothing_annotated() throws Exception {
+    DefaultValidationGroupResolver r =
+        new DefaultValidationGroupResolver(new Class<?>[] {Default.class}, true);
+    Method m = Plain.class.getMethod("run");
+    assertThat(r.resolveGroups(new Plain(), m)).containsExactly(Default.class);
+    assertThat(r.shouldValidateReturnValue(new Plain(), m)).isTrue();
+  }
+
+  @Test
+  void class_annotation_overrides_constructor_default() throws Exception {
+    DefaultValidationGroupResolver r =
+        new DefaultValidationGroupResolver(new Class<?>[] {Default.class}, true);
+    Method m = ClassAnnotated.class.getMethod("run");
+    assertThat(r.resolveGroups(new ClassAnnotated(), m)).containsExactly(GroupA.class);
+    assertThat(r.shouldValidateReturnValue(new ClassAnnotated(), m)).isTrue();
+  }
+
+  @Test
+  void method_annotation_overrides_class_annotation() throws Exception {
+    DefaultValidationGroupResolver r =
+        new DefaultValidationGroupResolver(new Class<?>[] {Default.class}, true);
+    Method m = ClassAnnotated.class.getMethod("other");
+    assertThat(r.resolveGroups(new ClassAnnotated(), m)).containsExactly(GroupB.class);
+    assertThat(r.shouldValidateReturnValue(new ClassAnnotated(), m)).isFalse();
+  }
+
+  @Test
+  void empty_groups_on_annotation_normalize_to_constructor_default() throws Exception {
+    DefaultValidationGroupResolver r =
+        new DefaultValidationGroupResolver(new Class<?>[] {GroupA.class}, true);
+    Method m = EmptyGroups.class.getMethod("run");
+    assertThat(r.resolveGroups(new EmptyGroups(), m)).containsExactly(GroupA.class);
+  }
+
+  @Test
+  void constructor_default_for_return_value_used_when_no_annotation() throws Exception {
+    DefaultValidationGroupResolver r =
+        new DefaultValidationGroupResolver(new Class<?>[] {Default.class}, false);
+    Method m = Plain.class.getMethod("run");
+    assertThat(r.shouldValidateReturnValue(new Plain(), m)).isFalse();
+  }
+
+  @Test
+  void null_target_uses_method_declaring_class_for_class_annotation() throws Exception {
+    DefaultValidationGroupResolver r =
+        new DefaultValidationGroupResolver(new Class<?>[] {Default.class}, true);
+    Method m = ClassAnnotated.class.getMethod("run");
+    assertThat(r.resolveGroups(null, m)).containsExactly(GroupA.class);
+  }
+}
